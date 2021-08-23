f34c8c23535e47120da37799c09a99718a6c127c
MineralWater321 <paler.edward@gmail.com>
2021-08-23 05:45:00 +0000

    finalized swertres def(initial)
    still need rambol parameter, but not needed

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-# import random
-# def swertres(wager, number):
-# 	result = random.randint(1,999)
+import random
+def swertres(wager, number):
+	result = random.randint(1,999)
+	number_list = list(str(number))
+	result_list = list(str(result))
+	print("The result is " + str(result))
+	while len(number_list) < 3:
+		number_list.insert(0, 0)
+	while len(result_list) < 3:
+		result_list(0, 0)
+	if number == result:
+		return wager * 80
+	elif number_list == result_list:
+		return (wager/6) * 80
+	else:
+		print("No win")
+		return 0
 	
-# 	res_str = str(result)
-# 	bet_str = str(number)
-# 	print(res_str)
-# 	if number == result:
-# 		return wager * 80
-# 	elif res_str == bet_str:
-# 		return (wager/6) * 80
-# 	else:
-# 		print("No win")
-# 		return 0
-	
-# swertres(321, 8)
-
-s1 = str(123)
-s2 = str(12)
-set1 = s1.split(" ")
-set2 = s2.split(" ")
-# while len(set2) < 3:
-# 	set2.insert(0, 0)
-print(set1)
-print(set2)
-print(set1 == set2)
+swertres(321, 8)
